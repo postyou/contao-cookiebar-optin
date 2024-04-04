60a1c445f6721484fa88cfc50f303be85d22912b
MarkejN <markus.nestmann@outlook.com>
2024-04-04 10:23:39 +0200

    Add more details to the pageModel

--- src/Controller/CookiebarOptinController.php
+++ src/Controller/CookiebarOptinController.php
@@ -15,17 +15,27 @@ namespace Postyou\ContaoCookiebarOptin\Controller;
 use Contao\ContentModel;
 use Contao\Controller;
 use Contao\CoreBundle\Controller\AbstractController;
-use Contao\CoreBundle\InsertTag\InsertTagParser;
+use Contao\CoreBundle\Exception\NoLayoutSpecifiedException;
+use Contao\CoreBundle\Routing\ResponseContext\CoreResponseContextFactory;
+use Contao\CoreBundle\Util\LocaleUtil;
+use Contao\LayoutModel;
 use Contao\PageModel;
+use Contao\System;
+use Contao\ThemeModel;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\LocaleAwareInterface;
 
 #[Route('/_cookiebarOptin', defaults: ['_scope' => 'frontend'])]
 class CookiebarOptinController extends AbstractController
 {
     public function __construct(
-        private readonly InsertTagParser $insertTagParser,
+        private readonly LocaleAwareInterface $translator,
+
+        #[Autowire(service: 'contao.routing.response_context_factory')]
+        private readonly CoreResponseContextFactory $responseContextFactory,
     ) {}
 
     #[Route('/{optinId}', requirements: ['optinId' => '\d+'], methods: ['GET'])]
@@ -36,20 +46,52 @@ class CookiebarOptinController extends AbstractController
         $pageId = $request->query->getInt('pageId');
 
         $contentModel = ContentModel::findById($optinId);
-        $pageModel = PageModel::findById($pageId);
+        $pageModel = PageModel::findWithDetails($pageId);
 
         if (null === $contentModel || null === $pageModel) {
             return new Response();
         }
 
-        $GLOBALS['objPage'] = $pageModel;
+        $this->prepare($pageModel, $request);
 
         $contentModel->useCookiebarOptin = false;
 
         $content = Controller::getContentElement($optinId);
-        $content = $this->insertTagParser->replace($content);
-        $content = Controller::replaceDynamicScriptTags($content);
 
         return new Response($content);
+    }
+
+    private function prepare(PageModel $pageModel, Request $request): void
+    {
+        // Deprecated since Contao 4.0, to be removed in Contao 6.0
+        $GLOBALS['TL_LANGUAGE'] = LocaleUtil::formatAsLanguageTag($pageModel->language);
+
+        $locale = LocaleUtil::formatAsLocale($pageModel->language);
+
+        $request->setLocale($locale);
+        $this->translator->setLocale($locale);
+
+        System::loadLanguageFile('default');
+
+        $layoutModel = LayoutModel::findById($pageModel->layout);
+
+        if (null === $layoutModel) {
+            throw new NoLayoutSpecifiedException('No layout specified');
+        }
+
+        // Store the layout ID
+        $pageModel->layoutId = $layoutModel->id;
+
+        // Set the layout template and template group
+        $pageModel->template = $layoutModel->template ?: 'fe_page';
+        $pageModel->templateGroup = '';
+
+        if ($themeModel = ThemeModel::findById($layoutModel->pid)) {
+            $pageModel->templateGroup = $themeModel->templates;
+        }
+
+        $this->responseContextFactory->createContaoWebpageResponseContext($pageModel);
+
+        $GLOBALS['objPage'] = $pageModel;
     }
 }
